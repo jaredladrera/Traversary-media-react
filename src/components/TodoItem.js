2858eccc0d5d8015a050d4bffd41b180d9ccbdd4
lance jared <lance21@gmail.com>
2020-03-09 21:27:12 +0800

    react by traversary media

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class TodoItem extends Component {
+    render() {
+        console.log(this.props.todox)
+        return (
+            <div>
+                <h2>Hello</h2>
+            </div>
+        )
+    }
+}
+
+export default TodoItem
---
+++ src/components/Todos.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem'
+
+
+class Todos extends Component {
+
+    render() { 
+       // console.log(this.props.todos);
+        return  this.props.todos.map(todo => 
+                    <TodoItem key={todo.id} todox={todo} />
+                )
+          
+         
+    }
+}
+ 
+export default Todos;
